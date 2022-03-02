ab69c9a072f210712d3b7b8e8ffeba9fb6936f78
BuildTools <unconfigured@null.spigotmc.org>
2022-03-03 00:13:04 +0100

    Creación de archivos y carpetas principales

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$ruta = explode('/', $_GET['ruta'] ?? '');
+
+$controlador = "controladores/$ruta[0].php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php include 'vistas/header.php' ?>
+
+    <?php include file_exists($controlador) ? $controlador : 'vistas/404.php' ?>
+
+    <?php include 'vistas/footer.php' ?>
+</body>
+
+</html>
